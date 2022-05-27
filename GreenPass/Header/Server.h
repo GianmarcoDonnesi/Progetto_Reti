d162a2562796c9de52bdb4d6f5b420ea5d5d31d2
GianmarcoDonnesi <gianmarco.donnesi001@studenti.uniparthenope.it>
2022-05-27 11:50:03 +0200

    new

--- GreenPass/Header/Server.h
+++
@@ -1,7 +0,0 @@
-#include "include_functions.h"
-#include "../libreria/db_model.h"
-
-void invioRichiestaS(int sfd, int sfd1);
-void invioRichiestaT(int sfd, int sfd1);
-void rispostaT(int sfd,char identificativo[4]);
-void rispostaS(int sfd);
